7c833b31e1c1b7c956a520b08623724918689264
fpavfer442 <fpavfer442@g.educaand.es>
2022-05-08 10:38:16 +0200

    Force entity check at startup
    The added annotation forces the check of entities in the entities package

--- src/main/java/com/mediahub/back/MediahubBackApplication.java
+++ src/main/java/com/mediahub/back/MediahubBackApplication.java
@@ -2,7 +2,9 @@ package com.mediahub.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.mediahub.entities")
 @SpringBootApplication
 public class MediahubBackApplication {
 
