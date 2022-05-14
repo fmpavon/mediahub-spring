81ced4df14711fdccd10baf11ecbce302ac1abb7
fpavfer442 <fpavfer442@g.educaand.es>
2022-05-14 20:28:02 +0200

    findByUsername in the User repository now only returns a single user
    As the username is the new primary key for users, now we only get a single user instead of a list

--- src/main/java/com/mediahub/repositories/UserRepo.java
+++ src/main/java/com/mediahub/repositories/UserRepo.java
@@ -1,13 +1,11 @@
 package com.mediahub.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mediahub.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Long>{
 	
-	List<User> findByUsername(String username);
+	User findByUsername(String username);
 	
 }
