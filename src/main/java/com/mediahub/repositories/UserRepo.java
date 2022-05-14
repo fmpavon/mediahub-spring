10878e725dc0982504fe1921e1ade75e4544cf13
fpavfer442 <fpavfer442@g.educaand.es>
2022-05-14 20:33:45 +0200

    Fixed User repo repository identification
    Fixed primary key from type Long to type String

--- src/main/java/com/mediahub/repositories/UserRepo.java
+++ src/main/java/com/mediahub/repositories/UserRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mediahub.entities.User;
 
-public interface UserRepo extends JpaRepository<User, Long>{
+public interface UserRepo extends JpaRepository<User, String>{
 	
 	User findByUsername(String username);
 	
