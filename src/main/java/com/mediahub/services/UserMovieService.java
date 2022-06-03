7ed6f24b47cc16e0cdde924924759f8a0213b85f
fpavfer442 <fpavfer442@g.educaand.es>
2022-06-03 17:24:00 +0200

    Added service interface and service for UserMovie entity

---
+++ src/main/java/com/mediahub/services/UserMovieService.java
@@ -0,0 +1,47 @@
+package com.mediahub.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mediahub.entities.UserMovie;
+import com.mediahub.repositories.UserMovieRepo;
+
+@Service
+public class UserMovieService implements UserMovieServiceI{
+
+	@Autowired
+	private UserMovieRepo userMovieRepo;
+	
+	@Override
+	public List<UserMovie> getMovies() {
+		return userMovieRepo.findAll();
+	}
+
+	@Override
+	public UserMovie getUserMovieById(long id) {
+		return userMovieRepo.getById(id);
+	}
+
+	@Override
+	public UserMovie addUserMovie(UserMovie userMovie) {
+		return userMovieRepo.save(userMovie);
+	}
+
+	@Override
+	public UserMovie updateUserMovie(UserMovie userMovie) {
+		return userMovieRepo.save(userMovie);
+	}
+
+	@Override
+	public void removeUserMovie(UserMovie userMovie) {
+		userMovieRepo.delete(userMovie);
+	}
+
+	@Override
+	public void removeUserMovieById(long id) {
+		userMovieRepo.deleteById(id);
+	}
+
+}
---
+++ src/main/java/com/mediahub/services/UserMovieServiceI.java
@@ -0,0 +1,20 @@
+package com.mediahub.services;
+
+import java.util.List;
+
+import com.mediahub.entities.UserMovie;
+
+public interface UserMovieServiceI {
+	
+	public List<UserMovie> getMovies();
+	
+	public UserMovie getUserMovieById(long id);
+		
+	public UserMovie addUserMovie(UserMovie userMovie);
+	
+	public UserMovie updateUserMovie(UserMovie userMovie);
+	
+	public void removeUserMovie(UserMovie userMovie);
+	
+	public void removeUserMovieById(long id);
+}
