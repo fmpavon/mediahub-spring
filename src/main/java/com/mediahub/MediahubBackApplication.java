ac726905f284643fb691400faced2ea4fc986cbf
fpavfer442 <fpavfer442@g.educaand.es>
2022-06-07 17:26:00 +0200

    Removed unused imports in MediahubBackApplication

--- src/main/java/com/mediahub/MediahubBackApplication.java
+++ src/main/java/com/mediahub/MediahubBackApplication.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.mediahub.entities.Movie;
 import com.mediahub.entities.User;
