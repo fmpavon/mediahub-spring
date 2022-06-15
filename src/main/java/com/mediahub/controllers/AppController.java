918d1af6142ac82bca75f39fff5b07544e5a701c
fpavfer442 <fpavfer442@g.educaand.es>
2022-06-15 17:38:27 +0200

    Organized imports (small code cleanup)

--- src/main/java/com/mediahub/controllers/AppController.java
+++ src/main/java/com/mediahub/controllers/AppController.java
@@ -8,14 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.mediahub.classes.UserRole;
 import com.mediahub.entities.Movie;
